543078d5d421619340dde2a7286e147b5742bae4
pospispa <ppospisi@redhat.com>
2018-03-06 16:36:42 +0100

    Fix: External Dynamic Manila Provisioner; described file purpose

--- openstack-sharedfilesystems/pkg/sharedfilesystems/microversions.go
+++ openstack-sharedfilesystems/pkg/sharedfilesystems/microversions.go
@@ -14,6 +14,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+// Functions in this file shall ideally be moved into the github.com/gophercloud/gophercloud library
+
 package sharedfilesystems
 
 import (
